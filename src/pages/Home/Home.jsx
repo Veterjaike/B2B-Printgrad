922bf3e4098b9050f1904dd9260bca2dac249f7f
OlegPark <oleg.pak1996@gmail.com>
2025-06-27 15:01:14 +0300

    Добавил анимированный блок как на сайте принтграда

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import MainService from "../../components/MainService"
 import Portnair from "../../components/Portnair"
+import HomeBanner from "../../components/home/HomeBanner"
 
 export const Home = () => {
     return (
         <div className="home-content">
             <MainService />
+            <HomeBanner />
             <Portnair />
         </div>
     )
