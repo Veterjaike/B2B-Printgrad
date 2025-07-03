abfd8e2b0419192390674f0d97d22033bf6bf91c
Veterjaike <73855368+Veterjaike@users.noreply.github.com>
2025-07-03 15:13:33 +0300

    1

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import MainService from "../../components/home/MainService";
 import ZaBiznes from "../../components/home/ZaBiznes";
 import Steps from "../../components/home/Steps";
-import Leaders from "../../components/home/leaders";
+import Leaders from "../../components/home/Leaders";
 import Horizons from "../../components/home/Horizons";
 
 export const Home = () => {
