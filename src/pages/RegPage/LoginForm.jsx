1bb6f304c255e0d56a02cfd78842f81f6bc5b07a
Veterjaike <73855368+Veterjaike@users.noreply.github.com>
2025-07-03 14:22:46 +0300

    Revert "api"
    This reverts commit 0aff8fb02b752229a001e69616272281c699fe18.

--- src/pages/RegPage/LoginForm.jsx
+++ src/pages/RegPage/LoginForm.jsx
@@ -12,7 +12,7 @@ export default function LoginForm () {
         e.preventDefault();
 
         try {
-            const response = await fetch('https://b2b.printgrad.ru/api/login', {
+            const response = await fetch('https://b2b.printgrad.ru:3000/api/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password })
