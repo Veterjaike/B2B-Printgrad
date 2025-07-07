bd392a7776385b7303daa35d576b78487ec07212
Veterjaike <73855368+Veterjaike@users.noreply.github.com>
2025-07-07 12:48:48 +0300

    button role createorder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css'
+import './Header.css';
+
+function parseJwt(token) {
+  try {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split('')
+        .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+        .join('')
+    );
+    return JSON.parse(jsonPayload);
+  } catch {
+    return null;
+  }
+}
 
 export default function Header() {
-    return (
-        <header className='header'>
-            <div className="container">
-                <div className="header__left">
-                    <a className="header__left-link" href='/'>
-                        <img className="header__left-link-image" src="/images/logoZabiznes.png"
-                            alt="logo" width={200} height={68} />
-                    </a>
-                    <nav className='header__nav'>
-                        <Link className='header__nav-link btn' to="/" >Главная</Link>
-                        <Link className='header__nav-link btn' to="/orders" >Все заказы</Link>
-                        <Link className='header__nav-link btn' to="/myorders" >Мои заказы</Link>
+  const token = localStorage.getItem('token'); // Замени, если у тебя другой ключ для токена
+  const user = token ? parseJwt(token) : null;
 
-                    </nav>
-                </div>
-                <Link className='header__nav-link btn' to="/profile">Профиль</Link>
-            </div>
-        </header>
-    );
+  return (
+    <header className='header'>
+      <div className="container">
+        <div className="header__left">
+          <a className="header__left-link" href='/'>
+            <img
+              className="header__left-link-image"
+              src="/images/logoZabiznes.png"
+              alt="logo"
+              width={200}
+              height={68}
+            />
+          </a>
+          <nav className='header__nav'>
+            <Link className='header__nav-link btn' to="/" >Главная</Link>
+            <Link className='header__nav-link btn' to="/orders" >Все заказы</Link>
+            <Link className='header__nav-link btn' to="/myorders" >Мои заказы</Link>
+          </nav>
+        </div>
+
+        <div className="header__right">
+          {user?.role === 'заказчик' && (
+            <Link className='header__nav-link btn' to="/create-order">
+              Создать заявку
+            </Link>
+          )}
+          <Link className='header__nav-link btn' to="/profile">Профиль</Link>
+        </div>
+      </div>
+    </header>
+  );
 }
