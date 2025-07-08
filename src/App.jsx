e3f74e1b0d12fc74239bc1f234d999724e5be4f0
Veterjaike <73855368+Veterjaike@users.noreply.github.com>
2025-07-08 14:06:53 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import ProtectedRoute from './pages/RegPage/ProtectedRoute';
-import AdminRoute from './pages/RegPage/AdminRoute'; // 👈 добавлено
+import AdminRoute from './components/AdminRoute'; // 👈 добавлено
 import Header from './components/Header';
 import './App.css';
 import { Home, Profile } from './pages';
